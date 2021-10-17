e698c7416ec4585bcaaedec86c125947ef33e726
Lucas Ramos <lucas2adriano@gmail.com>
2021-10-17 17:53:47 -0200

    Consumindo banco de dados na web

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Linq;
+using WebPets.Models;
+
+namespace WebPets.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext Context;
+
+        public HomeController(ILogger<HomeController> logger,AppDbContext context)
+        {
+            Context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var pets = Context.Pets.ToList();
+            return View(pets);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
